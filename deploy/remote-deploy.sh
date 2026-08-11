b3d6e860529b8ea2afc544cef0ef874cc2cde7b8
Roy Wenman <doihaveto@gmail.com>
2026-08-11 23:07:26 +0100

    Set up Github Action

---
+++ deploy/remote-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+APP_DIR=~/www/roywenman.co.uk/apps/barcode-scan
+PUBLIC_DIR=~/www/roywenman.co.uk/public_html/projects/barcode-scan
+
+cd "$APP_DIR"
+
+composer install --no-dev --optimize-autoloader --no-interaction
+php artisan migrate --force
+
+rm -rf "${PUBLIC_DIR:?}"/*
+cp -r public/. "$PUBLIC_DIR/"
+sed -i "s|__DIR__\\.'/\\.\\./|__DIR__.'/../../../apps/barcode-scan/|g" "$PUBLIC_DIR/index.php"
+
+echo "Deploy complete"
